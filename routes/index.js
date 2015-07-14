82266bdb10c0e4a88c2a456830a47b6349de1775
Daniel Goldenberg <daniel.aron@goldenberg.co>
2015-07-14 17:01:51 -0300

    if logged redirect to fonkapps list

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,21 @@ var isAuthenticated = function (req, res, next) {
 	res.redirect('/');
 }
 
+var isNotAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (!req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/fonkapps');
+}
+
+
 module.exports = function(passport){
 
 	/* GET login page. */
-	router.get('/', function(req, res) {
+	router.get('/', isNotAuthenticated, function(req, res) {
     	// Display the Login page with any flash message, if any
 		res.render('index', { message: req.flash('message') });
 	});
