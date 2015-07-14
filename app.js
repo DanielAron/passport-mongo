7a3eeed22e3d9695778c52102d1aad036394ca23
Daniel Goldenberg <daniel.aron@goldenberg.co>
2015-07-14 18:29:24 -0300

    Add sign out button

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ var routes = require('./routes/index')(passport);
 app.use('/', routes);
 app.use('/fonkapps', fonkapps);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
